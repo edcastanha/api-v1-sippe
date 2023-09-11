0421d7f981ae755eeffe48d2f72ea9c120f78e51
edcastanha <edcastanha@gmail.com>
2023-09-11 00:33:52 -0300

    Ajuste para celery e nginx

--- secedu-dj-api/setup.py
+++ secedu-dj-api/setup.py
@@ -10,13 +10,8 @@ def create_superuser():
     username = 'edson.filho'
     email = 'edson.filho@secedu.com.br'
     password = 'mudar1234'
-    # Executando migrate
-    os.system('python manage.py migrate --noinput | exit 1')
     # Criando superusuário
     if not User.objects.filter(username=username).exists():
-        os.system('python manage.py makemigrations  --noinput | exit 1')
-        os.system('python manage.py migrate --noinput | exit 1')
-        os.system('python manage.py collectstatic  --noinput | exit 1')
         User.objects.create_superuser(username=username, email=email, password=password)
         print('Superusuário criado - Sucesso!')     
     else:
