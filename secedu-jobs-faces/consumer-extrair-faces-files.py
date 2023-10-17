141eee93b63a75755813ee860e04b1780b6278c2
edcastanha <edcastanha@gmail.com>
2023-10-17 01:57:31 -0300

    Fix: Ajuste de SERVER e data['key'] para conexoes e troca de dados.

--- secedu-jobs-faces/consumer-extrair-faces-files.py
+++ secedu-jobs-faces/consumer-extrair-faces-files.py
@@ -26,7 +26,7 @@ ROUTE_KEY='extractor'
 QUEUE_CONSUMER='ftp'
 ASK_DEBUG = False
 
-DIR_CAPS ='capturas'
+DIR_CAPS ='media/capturas'
 
 BACKEND_DETECTOR='retinaface'
 MODEL_BACKEND ='mtcnn'
@@ -36,11 +36,11 @@ from os import environ
 
 r = redis.StrictRedis(host=REDIS_SERVER, port=6379, db=0)
 
-logger.info(f' <**_ 1 _**> RMQ_SERVER::{RMQ_SERVER}')
+logger.debug(f' <**_ 1 _**> RMQ_SERVER::{RMQ_SERVER}')
 
 class ConsumerExtractor:
     def __init__(self):
-        logger.info(f' <**_ INIT _**>')
+        logger.debug(f' <**_ INIT _**>')
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(
                 host=RMQ_SERVER,
@@ -54,10 +54,10 @@ class ConsumerExtractor:
             exchange=EXCHANGE,
             routing_key=ROUTE_KEY
         )
-        logger.info(f' <**_**> ConsumerExtractor: {self.connection}')
+        logger.debug(f' <**_**> ConsumerExtractor: INIT')
 
     def run(self):
-        logger.info(f' <**_**> ConsumerExtractor: Aguardando {QUEUE_CONSUMER}')
+        logger.debug(f' <**_**> ConsumerExtractor: Aguardando {QUEUE_CONSUMER}')
         self.channel.basic_consume(
             queue=QUEUE_CONSUMER,
             on_message_callback=self.process_message,
@@ -66,15 +66,17 @@ class ConsumerExtractor:
 
         try:
             self.channel.start_consuming()
-            logger.info(f' <**_**> ConsumerExtractor: start_consumer')
+            logger.debug(f' <**_**> ConsumerExtractor: start_consumer')
         finally:
             self.connection.close()
-            logger.info(f' <**_**> ConsumerExtractor: close')
+            logger.debug(f' <**_**> ConsumerExtractor: close')
 
     def process_message(self, ch, method, properties, body):
         data = json.loads(body)
         file = data['path_file']
-        logger.info(f' <**_**> ConsumerExtractor: process_message')
+        horario = data['horario']
+        device = data['local']
+        logger.debug(f' <**_**> ConsumerExtractor: process_message:: {data}')
 
         if file.lower().endswith(('.jpg', '.jpeg', '.png')):
             now = dt.now()
@@ -84,44 +86,35 @@ class ConsumerExtractor:
             message_dict = {
                 'data_processo': proccess,
                 'data_captura': data_captura,
-                'hora_captura': data['horario'],
+                'hora_captura': horario,
                 'nome_equipamento': equipamento,
-                'local_equipamento': data['local'],
+                'local_equipamento': device,
                 'captura_base': file,
             }
+            
+            logger.debug(f' <**_**> ConsumerExtractor: process_message:: {message_dict}')
             face_objs = DeepFace.extract_faces(img_path=file,
                                                detector_backend=BACKEND_DETECTOR,
-                                               enforce_detection=False,  
+                                               enforce_detection=False,
                                                align=True
                                                )
-            # Extrair a parte da URL que contém HH, MM e SS
-        #    matchM = re.search(r'/(\d{2})/(\d{2})/(\d{2})', file)
-        #    if matchM:
-        #        hh, mm, ss = matchM.groups()
-        #        logger.info(f' <**_**> ConsumerExtractor: Math - HH/MM/SS')
+            try:
+                for index, face_obj in enumerate(face_objs):
+                    if face_obj['confidence'] >= LIMITE_DETECTOR:
+                        face = face_obj['face']
+                        new_face = os.path.join(str(DIR_CAPS), str(equipamento), str(data_captura), str(horario))
 
-            #Extrair a parte da URL que contém HH, MM e SS
-        #    matchP = re.search(r'/(\d{2})/(\d{2})\.(\d{2})', file)
-        #    if matchP:
-        #        hh, mm, ss = matchP.groups()
-        #        logger.info(f' <**_**> ConsumerExtractor: Matc - HH/MM.SS')
+                        if not os.path.exists(new_face):
+                            os.makedirs(new_face, exist_ok=True)
 
-            for index, face_obj in enumerate(face_objs):
-                if face_obj['confidence'] >= LIMITE_DETECTOR:
-                    face = face_obj['face']
-                    new_face = os.path.join(DIR_CAPS, equipamento, data_captura, hh,mm,ss)
-
-                    if not os.path.exists(new_face):
-                        os.makedirs(new_face, exist_ok=True)
-
-                    # Converta a imagem de float32 para uint8 (formato de imagem)
-                    face_uint8 = (face * 255).astype('uint8')
-                    
-                    # Gere um nome de arquivo único para a face
-                    save_path = os.path.join(new_face, f"face_{index}.jpg")
-                    logger.info(f' <**SAVE NEW FACE**> Path:: {save_path}: ')
-                    
-                    try:
+                        # Converta a imagem de float32 para uint8 (formato de imagem)
+                        face_uint8 = (face * 255).astype('uint8')
+                        
+                        # Gere um nome de arquivo único para a face
+                        save_path = os.path.join(new_face, f"face_{index}.jpg")
+                        logger.debug(f' <**SAVE NEW FACE**> Path:: {save_path}: ')
+                        
+                        
                         # Salve a face no diretório "captura/" usando OpenCV
                         cv2.imwrite(save_path, cv2.cvtColor(face_uint8, cv2.COLOR_RGB2BGR))
 
@@ -134,9 +127,9 @@ class ConsumerExtractor:
                         pipeline = r.pipeline(transaction=False)
                         pipeline.hset(save_path, mapping={"message": message_str})
                         pipeline_results = pipeline.execute()
-                        logger.info(f' <**_ REDIS _**> PIPELINE :: {pipeline_results}')
-                    except Exception as e:
-                     logger.info(f' <**EXTRATOR FACE**> Erro:: {e}')
+                        logger.debug(f' <**_ REDIS _**> PIPELINE :: {pipeline_results}')
+            except Exception as e:
+                logger.debug(f' <**EXTRATOR FACE**> Erro:: {e}')
 
 if __name__ == "__main__":
     job = ConsumerExtractor()
