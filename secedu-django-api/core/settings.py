f67412223df1d85b5a42ed8a10ee072feba23c34
edcastanha <edcastanha@gmail.com>
2023-10-29 22:15:57 -0300

    fix: staicfiles import

--- secedu-django-api/core/settings.py
+++ secedu-django-api/core/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS =os.environ.get('ALLOWED_HOSTS').split(',')
 INSTALLED_APPS = [
     # General use templates & template tags (should appear first)
     'jazzmin',
-
+    #Default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
