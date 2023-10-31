37c6e837a15a7728aacb2280dc76c0f29a7f50db
edcastanha <edcastanha@gmail.com>
2023-10-31 17:37:40 -0300

    feat: Login App Admin

--- secedu-django-api/core/cameras/views.py
+++ secedu-django-api/core/cameras/views.py
@@ -44,4 +44,4 @@ def total_faces_por_dia(request):
 @login_required(login_url="login")
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def backend(request):
-    return render(request, "backend.html")
+    return render(request, "app/backend.html")
