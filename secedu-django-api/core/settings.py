394a7ebb0f5f3cc35705983164e63c45ffc92922
edcastanha <edcastanha@gmail.com>
2023-10-31 23:55:28 -0300

    fix: ajustes de estaticos

--- secedu-django-api/core/settings.py
+++ secedu-django-api/core/settings.py
@@ -166,9 +166,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #CELERY SETTINGS
